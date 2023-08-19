75149afabc8f1553a948713220775065dd649491
Sicambr <sicambr84@gmail.com>
2023-08-20 01:31:26 +0700

    changed gitignore file

--- Igorek.py
+++ Igorek.py
@@ -3,7 +3,7 @@ import os
 import to_s191
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit
 from PyQt5 import QtWidgets, uic
 from UnIgor import Ui_MainWindow
 
@@ -15,10 +15,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         current_path = to_s191.load_path()
         self.show_path.setText(current_path)
         self.start_button.pressed.connect(
-            lambda: self.start_button_pressed(current_path))
+            lambda: self.start_button_pressed())
 
 
-    def start_button_pressed(self, current_path):
+    def start_button_pressed(self):
+        current_path=self.show_path.text()
         self.status_label.setText(
             'В работе по созданию новых .NC файлов. Ждем...')
         to_s191.main(current_path)
